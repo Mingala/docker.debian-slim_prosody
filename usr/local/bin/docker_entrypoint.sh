f3a2ca32ba508974f2cb582c6316b4365268a8d1
Mingala <lcambou@wanadoo.fr>
2020-04-23 18:41:38 +0100

    prosody start in foreground

--- usr/local/bin/docker_entrypoint.sh
+++ usr/local/bin/docker_entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # run Sasl service
 /etc/init.d/saslauthd start
-# run Prosody service
-/etc/init.d/prosody start
+# run Prosody as prosody user in foreground
+usermod -u "$(stat -c %u /var/lib/prosody/.)" prosody
+runuser -u prosody -- "prosody" "-F"
