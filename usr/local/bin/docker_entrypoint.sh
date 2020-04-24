0ae91a2e5daf11224d613662d8ac4f273934b8c3
Mingala <lcambou@wanadoo.fr>
2020-04-24 08:58:45 +0100

    Update docker_entrypoint.sh

--- usr/local/bin/docker_entrypoint.sh
+++ usr/local/bin/docker_entrypoint.sh
@@ -2,5 +2,4 @@
 # run Sasl service
 /etc/init.d/saslauthd start
 # run Prosody as prosody user in foreground
-usermod -u "$(stat -c %u /var/lib/prosody/.)" prosody
 runuser -u prosody -- "prosody" "-F"
